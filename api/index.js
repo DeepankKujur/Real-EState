8ca96cded3eb8caed0baeddfacf39fba526f8af0
Gaurav Singh <gauravsinghchunar007@gmail.com>
2024-12-12 18:04:30 +0530

    database connection is successfully done

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+//here we are configuring the dotenv package and connecting to the database
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("connected to database");
+}).catch((err) => {
+  console.log(err);
+});
+
+
+//here we have created the server
 const app = express();
 app.listen(3000, () => {
   console.log("server is running on port 3000 ");
 });
+
