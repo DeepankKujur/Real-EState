c7298f0f1d65bc0f624fef090b20d9df7fba531f
Gaurav Singh <gauravsinghchunar007@gmail.com>
2024-12-17 12:44:17 +0530

    update listing api route is created

--- api/contollers/listing.controller.js
+++ api/contollers/listing.controller.js
@@ -1,4 +1,6 @@
+import mongoose from 'mongoose';
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -23,6 +25,37 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json('Listing has been deleted!');
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+
+export const updateListing = async (req, res, next) => {
+  try {
+    // Validate and convert req.params.id to ObjectId
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return next(errorHandler(400, 'Invalid listing ID format.'));
+    }
+
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found!'));
+    }
+
+    if (req.user.id !== listing.userRef.toString()) {
+      return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true } 
+    );
+
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
