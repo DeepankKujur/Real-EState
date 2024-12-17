ad08713088e39490485673103d1ad9ec18756d30
Gaurav Singh <gauravsinghchunar007@gmail.com>
2024-12-17 14:28:04 +0530

    search api route is created

--- api/contollers/listing.controller.js
+++ api/contollers/listing.controller.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
@@ -7,52 +7,50 @@ export const createListing = async (req, res, next) => {
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
 
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
 
   if (!listing) {
-    return next(errorHandler(404, 'Listing not found!'));
+    return next(errorHandler(404, "Listing not found!"));
   }
 
   if (req.user.id !== listing.userRef) {
-    return next(errorHandler(401, 'You can only delete your own listings!'));
+    return next(errorHandler(401, "You can only delete your own listings!"));
   }
 
   try {
     await Listing.findByIdAndDelete(req.params.id);
-    res.status(200).json('Listing has been deleted!');
+    res.status(200).json("Listing has been deleted!");
   } catch (error) {
     next(error);
   }
 };
 
-
-
 export const updateListing = async (req, res, next) => {
   try {
     // Validate and convert req.params.id to ObjectId
     if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
-      return next(errorHandler(400, 'Invalid listing ID format.'));
+      return next(errorHandler(400, "Invalid listing ID format."));
     }
 
     const listing = await Listing.findById(req.params.id);
 
     if (!listing) {
-      return next(errorHandler(404, 'Listing not found!'));
+      return next(errorHandler(404, "Listing not found!"));
     }
 
     if (req.user.id !== listing.userRef.toString()) {
-      return next(errorHandler(401, 'You can only update your own listings!'));
+      return next(errorHandler(401, "You can only update your own listings!"));
     }
 
     const updatedListing = await Listing.findByIdAndUpdate(
       req.params.id,
       req.body,
-      { new: true, runValidators: true } 
+      { new: true, runValidators: true }
     );
 
     res.status(200).json(updatedListing);
@@ -60,3 +58,66 @@ export const updateListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let offer = req.query.offer;
+
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+// The $in operator is used to match any value from an array against a field in a document.
+// The $regex operator is used to match strings against a regular expression.It is useful for pattern matching in text fields.
+// Options:
+// i: Case-insensitive match.
+// m: Multiline match.
+// s: Allows . to match newline characters.
+// x: Ignores whitespace in the regex pattern.
+// l: Locale-dependent match.
+// u: Treats the pattern as a Unicode pattern.
+
