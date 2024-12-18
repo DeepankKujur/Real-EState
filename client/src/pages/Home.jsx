8b799d4930283d9d7a5b5ec29d055f8416563965
Gaurav Singh <gauravsinghchunar007@gmail.com>
2024-12-18 21:20:47 +0530

    issue solved

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from "swiper/react"
 import { Navigation } from 'swiper/modules';
 import SwiperCore from "swiper";
 import 'swiper/css/bundle';
-import ListingItem from '../components/ListingItem';
+import ListingItem from "../components/ListingItem";
 
 
 export default function Home() {
