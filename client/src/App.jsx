57056980a24521c25b03d1a2315a28a708f0b688
Gaurav <gauravsinghchunar007@gmail.com>
2025-05-15 16:52:48 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignOut from './pages/SignOut';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import Chatbot from './components/Chatbot';
 import PrivateRoute from './components/PrivateRoute';
 import Listing from './pages/Listing';
 import CreateListing from './pages/CreateListing';
